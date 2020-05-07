1abb6a86047ac95f471a3b2daa408e3a6c752e7b
Your Name <you@example.com>
2020-05-07 11:45:06 +0700

    Publishing to gh-page

---
+++ build/precache-manifest.8e9c1ac35ba419aa567ebe513fad8288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115ea144f1cbaad2c148dc0fd7b51763",
+    "url": "/Todo-Application/index.html"
+  },
+  {
+    "revision": "2f0fc76c308c8d6d989f",
+    "url": "/Todo-Application/static/css/main.45ab6bc9.chunk.css"
+  },
+  {
+    "revision": "6630a6d223a535ff2db0",
+    "url": "/Todo-Application/static/js/2.489c201f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-Application/static/js/2.489c201f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0fc76c308c8d6d989f",
+    "url": "/Todo-Application/static/js/main.b32bf8d0.chunk.js"
+  },
+  {
+    "revision": "ac814af9c843344b666a",
+    "url": "/Todo-Application/static/js/runtime-main.5c0dfae0.js"
+  }
+]);
